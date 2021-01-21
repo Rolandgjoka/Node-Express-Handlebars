b8b7eb6ce2a62b5fa872180fcccd63f3f05ba8f5
Roland Gjoka <rolandgjoka@hotmail.com>
2021-01-21 10:52:40 -0600

    fix

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,10 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+if(process.env.PORT){
+  var connection =  mysql.createConnection (process.env.JAWSDB_URL)
+
+}else{
+ 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -7,7 +12,7 @@ var connection = mysql.createConnection({
     password: "rootroot",
     database: "burgers_db"
   });
-  
+}
 
   connection.connect(function(err) {
     if (err) {
